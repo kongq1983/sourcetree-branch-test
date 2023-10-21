7f0808dec58bf15d5cdc1a3baf3ec19009a88bd7
kong <kongq1983@qq.com>
2023-10-21 23:51:42 +0800

    first commit

---
+++ src/main/java/com/kq/branch/BranchApplication.java
@@ -0,0 +1,16 @@
+package com.kq.branch;
+
+/**
+ * BranchApplication
+ *
+ * @author kq
+ * @date 2023-10-21 21:31
+ * @since 1.0.0
+ */
+public class BranchApplication {
+
+    public static void main(String[] args) {
+        System.out.println(BranchApplication.class);
+    }
+
+}
---
+++ src/main/java/com/kq/branch/Version1.java
@@ -0,0 +1,17 @@
+package com.kq.branch;
+
+import lombok.Data;
+
+/**
+ * Version1
+ *
+ * @author kq
+ * @date 2023-10-21 23:48
+ * @since 1.0.0
+ */
+@Data
+public class Version1 {
+
+    private String version1;
+
+}
